6158141d094e577b0a18bdb6b84cf24dd4abd154
DenisB7 <denzelbe1987@gmail.com>
2023-05-17 20:37:16 +0500

    Create register page

--- app_accounts/urls.py
+++ app_accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from app_accounts.views import MainView
+from app_accounts.views import MainView, RegisterView
 
 app_name = 'app_accounts'
 urlpatterns = [
     path('', MainView.as_view(), name="main"),
+    path('register', RegisterView.as_view(), name="register"),
     path("login", LoginView.as_view(template_name="app_accounts/login.html"), name="login"),
     path("logout", LogoutView.as_view(), name="logout"),
 ]
--- app_accounts/views.py
+++ app_accounts/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 
+from app_accounts.forms import UserCreationFormCustom
+from django.views.generic.edit import CreateView
 
 class MainView(View):
 
     def get(self, request):
         return render(request, 'main.html', {'username': request.user.username})
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationFormCustom
+    template_name = "app_accounts/register.html"
+    success_url = reverse_lazy('app_accounts:main')
