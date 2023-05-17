fde182f9e0010ab556cd2b191f555c828c0cc42b
DenisB7 <denzelbe1987@gmail.com>
2023-05-18 00:51:35 +0500

    Create survey view for survey page

--- app_main/views.py
+++ app_main/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class MainView(View):
 
     def get(self, request):
         return render(request, 'app_main/main.html', {'username': request.user.username})
+
+
+class SurveyView(LoginRequiredMixin, View):
+
+    def get(self, request):
+        return render(request, 'app_main/survey.html', {'username': request.user.username})
