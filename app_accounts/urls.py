216f8a221a31349be9adbf1d5023935aeb625443
DenisB7 <denzelbe1987@gmail.com>
2023-05-17 16:36:49 +0500

    Add login logout paths

--- app_accounts/urls.py
+++ app_accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from app_accounts.views import MainView
 
 app_name = 'app_accounts'
 urlpatterns = [
     path('', MainView.as_view(), name="main"),
+    path("login", LoginView.as_view(template_name="app_accounts/login.html"), name="login"),
+    path("logout", LogoutView.as_view(), name="logout"),
 ]
