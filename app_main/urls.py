8a7842b4e4426079bada858e41bc5177a2d1c60f
DenisB7 <denzelbe1987@gmail.com>
2023-05-18 00:52:27 +0500

    Add path to survey page

--- app_main/urls.py
+++ app_main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from app_main.views import MainView
+from app_main.views import MainView, SurveyView
 
 
 app_name = "app_main"
 urlpatterns = [
     path('', MainView.as_view(), name="main"),
+    path('survey', SurveyView.as_view(), name="survey"),
 ]
