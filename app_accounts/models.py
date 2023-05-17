c96834c12c5d7071bf49ba05ca9959134f839bb2
DenisB7 <denzelbe1987@gmail.com>
2023-05-17 15:17:50 +0500

    Add country field to user model

--- app_accounts/models.py
+++ app_accounts/models.py
@@ -4,7 +4,13 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    COUNTRY_CHOICES = (
+        ('us', 'USA'),
+        ('jp', 'Japan'),
+        ('uk', 'United Kingdom'),
+        ('sz', 'SWITZERLAND'),
+    )
+    country = models.CharField(max_length=5, choices=COUNTRY_CHOICES, blank=True)
 
     def __str__(self):
-        return f"USERNAME: {self.username} EMAIL: {self.email}"
+        return self.username
