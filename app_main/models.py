ff83fa6b8f9476cc5266ef8394bedf10af03fc83
DenisB7 <denzelbe1987@gmail.com>
2023-05-18 00:12:00 +0500

    Add company cruise model

--- app_main/models.py
+++ app_main/models.py
@@ -1,3 +1,13 @@
+from project_demo.settings import AUTH_USER_MODEL
 from django.db import models
 
-# Create your models here.
+
+class CompanyCruise(models.Model):
+    NAMES_CHOICES = (
+        ('storylines', 'storylines'),
+    )
+    name = models.CharField(max_length=20, choices=NAMES_CHOICES)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="company_cruise")
+
+    def __str__(self):
+        return f"{self.user.username}"
