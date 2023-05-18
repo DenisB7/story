6a69e477f32cced98a8ff060522096a26286b2de
DenisB7 <denzelbe1987@gmail.com>
2023-05-18 13:31:22 +0500

    Add documentation

--- app_accounts/models.py
+++ app_accounts/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    """
+    When you will have new model which you will need to link with User model,
+    please don't use standard User model for ForeignKey, since we modified it, take it from settings.py:
+
+    from project_demo.settings import AUTH_USER_MODEL
+
+    class AnotherModel(models.Model):
+        user = models.ForeignKey(AUTH_USER_MODEL, ...)
+    """
     COUNTRY_CHOICES = (
         ('us', 'USA'),
         ('jp', 'Japan'),
