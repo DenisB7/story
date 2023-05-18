30101f3f08498d409148b49de509e91e1d6817dc
DenisB7 <denzelbe1987@gmail.com>
2023-05-18 09:16:20 +0500

    Rename model

--- app_main/admin.py
+++ app_main/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from app_main.models import CompanyCruise
+from app_main.models import FavoriteCruiseCompany
 
 
-@admin.register(CompanyCruise)
-class CompanyCruiseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user')
-    list_filter = ('name', 'user')
+@admin.register(FavoriteCruiseCompany)
+class FavoriteCruiseCompanyAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'user', 'justification')
+    list_filter = ('company_name', 'user')
