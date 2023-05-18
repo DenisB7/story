dc2c0ea4881b395fa96ee2660a84784b98c39e63
DenisB7 <denzelbe1987@gmail.com>
2023-05-18 09:13:28 +0500

    Fix reverse to the main app

--- app_accounts/views.py
+++ app_accounts/views.py
@@ -9,7 +9,7 @@ from app_accounts.forms import UserCreationFormCustom
 class RegisterView(UserPassesTestMixin, CreateView):
     form_class = UserCreationFormCustom
     template_name = "app_accounts/register.html"
-    success_url = reverse_lazy('app_accounts:main')
+    success_url = reverse_lazy('app_main:main')
 
     def test_func(self):
         return self.request.user.is_anonymous
