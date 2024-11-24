e64d9a1afe76ed2b4400778e96ef404346b23036
DenisB7 <denzelbe1987@gmail.com>
2024-11-24 20:01:08 +0500

    Return repo and all what was built

--- app_main/models.py
+++ app_main/models.py
@@ -2,6 +2,10 @@ from project_demo.settings import AUTH_USER_MODEL
 from django.db import models
 
 
+def get_order_number(instance):
+    return instance.pk
+
+
 class FavoriteCruiseCompany(models.Model):
     NAMES_CHOICES = (
         ('storylines', 'storylines'),
@@ -9,6 +13,7 @@ class FavoriteCruiseCompany(models.Model):
     company_name = models.CharField(max_length=20, choices=NAMES_CHOICES)
     justification = models.TextField()
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="company_cruise")
+    order_number = models.PositiveBigIntegerField(default=get_order_number)
 
     def __str__(self):
         return f"USER {self.user.username} FAVORITE CRUISE COMPANY {self.company_name}"
